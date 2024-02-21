15d81c9a38fff49d6ecbc3e814d32e06f2a16b98
UkantiChandu <chandunitr@gmail.com>
2024-02-21 23:00:11 +0530

    Refactored code.

--- frequent char2.py
+++
@@ -1,7 +0,0 @@
-string=input("enter the string:")
-char=input("enter the character to find the frequency:")
-count=0
-for i in string:
-    if (i==char):
-        count=count+1
-print("the frequency of char in the string is:",count)
